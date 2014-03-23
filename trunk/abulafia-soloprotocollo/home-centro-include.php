48fc3af5d0b914e5c15499b25fe3b4b7f2ddaa06
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-03-23 13:10:42 +0000

    aggiunte info alla home

--- trunk/abulafia-soloprotocollo/home-centro-include.php
+++ trunk/abulafia-soloprotocollo/home-centro-include.php
@@ -8,13 +8,20 @@
 			Si evolvono gli uffici, pubblici e privati, per stare al passo con i tempi e con le rinnovate esigenze di gestione amministrativa
 			e del personale.<br><?php echo $_SESSION['nomeapplicativo'];?> vuole essere un piccolo contributo, aperto a chiunque abbia voglia di suggerire 
 			miglioramenti, per la gestione degli affari correnti delle segreterie dei volontari della CRI.
-			</p>
+			<br>Chiunque fosse interessato al progetto, puo' contattare l'amministratore del sito all'indirizzo email <strong><?php echo $_SESSION['email']; ?></strong></p>
+			<div class="text-right"><small>03 dicembre 2008</small></div>
 		</div>
 		
 		<div class="panel-heading">
 		<h3 class="panel-title"><strong>Stats:</strong></h3>
 		</div>
 		<div class="panel-body">
+		
+			<p><?php 
+                        $anniusoapplicazione = (strtotime("now") - strtotime("apr 1 2008"))/60/60/24/365;
+                        $giorniusoapplicazione = ((strtotime("now") - strtotime("apr 1 2008"))/60/60/24)-(int)$anniusoapplicazione*365;
+                        echo 'Questa web-application e\' in uso da '.(int)$anniusoapplicazione.' anni e '.(int)$giorniusoapplicazione.' giorni.';?></p>
+		
 			<p><?php 
 				$annoprotocollo = $_SESSION['annoprotocollo'];
 			
@@ -49,10 +56,10 @@
 		</div>
 		
 		<div class="panel-heading">
-		<h3 class="panel-title"><strong>Log degli ultimi 3 accessi:</strong></h3>
+		<h3 class="panel-title"><strong>Log degli ultimi 5 accessi:</strong></h3>
 		</div>
 		<div class="panel-body">
-			<p><?php $my_log -> publleggilog('1', '3', 'login', $_SESSION['logfile']); //legge dal log degli accessi ?></p>
+			<p><?php $my_log -> publleggilog('1', '5', 'login', $_SESSION['logfile']); //legge dal log degli accessi ?></p>
 		</div>
 		
 	</div>
