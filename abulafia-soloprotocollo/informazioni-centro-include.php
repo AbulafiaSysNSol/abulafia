fadc722b1756849f43140371836631d3f545b81c
alfiomusmarra <alfiomusmarra@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-03-23 16:10:02 +0000

    nomsg

--- abulafia-soloprotocollo/informazioni-centro-include.php
+++ abulafia-soloprotocollo/informazioni-centro-include.php
@@ -4,11 +4,31 @@
 		<h3 class="panel-title"><strong>Chi, come, quando</strong></h3>
 		</div>
 		<div class="panel-body">
-			<p><strong>STAFF:</strong> <?php echo $_SESSION['nomeapplicativo'];?> e' stato ideato da Biagio Saitta ed Alfio Musmarra e realizzato insieme a Roberto Parrinello, Alfio Costanzo, Laura Ferrara, Stefano Principato, Giovanni Susinni, Luigi Pistara', Laura Astolfi, Mara Basile e Marianna Schillaci.</p>
-			<p><strong>LICENZA D'USO:</strong> Tutto il sistema e' stato sviluppato con applicativi opensource quali (a solo titolo di esempio) Gedit, Gimp, Php, Mysql. A sua volta il codice di <?php echo $_SESSION['nomeapplicativo'];?> e' aperto a chiunque voglia contribuire allo sviluppo. E' anche possibile richiederne l'uso, che viene concesso gratuitamente per uso NO PROFIT a patto di citare la fonte, comunicarci eventuali migliorie apportate e renderle gratuitamente disponibili insieme al codice originario.</p>
-			<p><strong>GARANZIA:</strong> I creatori di <?php echo $_SESSION['nomeapplicativo'];?> sono a disposizione per risolvere qualsiasi problema dovesse presentarsi con l'uso dell'applicativo. Tuttavia questo viene fornito "AS IS", senza alcuna garanzia. Chi lo installa ne accetta le condizioni di licenza e lo usa sotto la propria esclusiva responsabilita'.</p>	
-			<p><strong>REQUISITI E COMPATIBILITA':</strong> <?php echo $_SESSION['nomeapplicativo'];?> e' stato pensato per poter essere 'portabile' e scalabile. Per eseguire l'applicativo e' sufficiente far girare, in locale o da remoto, un webserver in grado di interpretare il codice php. Sono quindi supportati sistemi operativi quali Linux, Mac, FreeBSD e Windows.</p>
-			<p><strong>CONTATTI:</strong> Per scriverci potete usare l'indirizzo email <strong><?php echo $_SESSION['email'];?></strong></p>
+			<p><strong>STAFF:</strong> 
+				<?php echo $_SESSION['nomeapplicativo'];?> e' stato inizialmente ideato da Biagio Saitta ed Alfio Musmarra
+				 ed oggi realizzato e mantenuto dagli stessi insieme a Federico D'urso e Stefano Principato.
+				 Preziose collaborazioni sono state fornite da Alfio Costanzo, Roberto Parrinello, 
+				 Laura Ferrara, Giovanni Susinni, Luigi Pistara', Laura Astolfi, Mara Basile e Marianna Schillaci.
+			</p>
+			<p><strong>LICENZA D'USO:</strong> Tutto il sistema e' stato sviluppato con software opensource quali 
+				(a solo titolo di esempio) Gedit, Gimp, Php, Mysql. A sua volta il 
+				codice di <?php echo $_SESSION['nomeapplicativo'];?> e' aperto a chiunque voglia contribuire
+				 allo sviluppo. E' anche possibile richiederne l'uso, che viene concesso gratuitamente per uso
+				  NO PROFIT a patto di citare la fonte, comunicarci eventuali migliorie apportate e renderle 
+				  gratuitamente disponibili insieme al codice originario.
+			</p>
+			<p><strong>GARANZIA:</strong> I creatori di <?php echo $_SESSION['nomeapplicativo'];?> 
+				sono disponibili a risolvere qualsiasi problema dovesse presentarsi con l'uso
+				 dell'applicativo. Tuttavia questo viene fornito "AS IS", senza alcuna garanzia.
+				  Chi lo installa ne accetta le condizioni di licenza e lo usa sotto la propria esclusiva responsabilita'.
+			</p>	
+			<p><strong>REQUISITI E COMPATIBILITA':</strong> <?php echo $_SESSION['nomeapplicativo'];?> e' stato pensato 
+					per poter essere 'portabile' e scalabile. Per eseguire l'applicativo e' sufficiente far
+					 girare, in locale o da remoto, un webserver in grado di interpretare il codice php. 
+					 Sono quindi supportati sistemi operativi quali Linux, Mac, FreeBSD e Windows.
+			</p>
+			<p><strong>CONTATTI:</strong> Per scriverci potete usare l'indirizzo email <strong><?php echo $_SESSION['email'];?></strong>
+			</p>
 
 		</div>
 		
