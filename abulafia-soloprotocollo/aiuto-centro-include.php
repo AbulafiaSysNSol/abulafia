53d0d02bf966ac5eac9a0cb6f64bac303d409bac
alfiomusmarra <alfiomusmarra@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-03-23 15:40:36 +0000

    nomsg

--- abulafia-soloprotocollo/aiuto-centro-include.php
+++ abulafia-soloprotocollo/aiuto-centro-include.php
@@ -4,13 +4,27 @@
 		<h3 class="panel-title"><strong>F.A.Q. - Frequently Ask Questions</strong></h3>
 		</div>
 		<div class="panel-body">
-			<p class="text-justify"><strong>Perche' non posso scrivere liberamente il nome di un mittente o di un destinatario quando protocollo, ma devo per forza inserirlo prima in anagrafica?</strong> <br> Spesso alcuni nomi o alcune cariche vengono inserite o abbreviate in forme differenti. Ad esempio, l'ispettore provinciale di trapani puo' essere inserito come isp. prov. trapani, ispettore prov. tp, isp. provinciale trapani. Per evitare inutili doppioni che darebbero inoltre risultati falsati nele ricerche, la fase di inserimento dell'anagrafica e' propedeutica e centralizzata rispetto all'inserimento del protocollo, della gestione del curriculum e delle altre funzioni della web application.</p>
+			<p class="text-justify"><strong>Perche' non posso scrivere liberamente il nome di un mittente
+			 				o di un destinatario quando protocollo, ma devo per forza 
+			 				inserirlo prima in anagrafica?
+			 			</strong> 
+			 			<br> Spesso alcuni nomi o alcune cariche vengono inserite o abbreviate
+			 			 in forme differenti. Ad esempio, l'ispettore provinciale di trapani 
+			 			 puo' essere inserito come isp. prov. trapani, ispettore prov. tp,
+			 			  isp. provinciale trapani. Per evitare inutili doppioni che darebbero
+			 			   inoltre risultati falsati nele ricerche, la fase di inserimento
+			 			    dell'anagrafica e' propedeutica e centralizzata rispetto all'inserimento
+			 			     del protocollo, della gestione del curriculum e delle altre funzioni
+			 			      della web application.
+			</p>
 			<br>
-			<p class="text-justify"><strong>Posso associare due o piu' documenti ad un protocollo?</strong> <br> Ogni protocollo puo' essere associato ad un solo file. Qualora fosse necessario associare piu' file, e' necessario inserire i documenti in un unico archivio (ad esempio rar, zip o tar.gz) che verra' poi associato al protocollo in questione.</p>
-			<br>
-			<p class="text-justify"><strong>Qual e' il limite massimo di upload delle foto per l'anagrafica e degli allegati per il protocollo?</strong> <br> Il limite massimo e' attualmente settato a 2 MegaByte dalla ditta che gestisce l'host e non e' modificabile dall'Ispettorato Regionale o da chi gestisce <?php echo $_SESSION['nomeapplicativo'];?>.</p>
-			<br>
-			<p class="text-justify"><strong>Cosa significa il motto in alto a destra nel sito?</strong> <br> E' una frase qualsiasi. Il vero motto e' sullo sfondo.</p>
+			<p class="text-justify"><strong>Posso associare due o piu' documenti ad un protocollo?
+						</strong> 
+						<br> Ogni protocollo puo' essere associato al momento ad un solo file. 
+						Qualora fosse necessario associare piu' file, e' necessario inserire i
+						 documenti in un unico archivio (ad esempio rar, zip o tar.gz) che verra'
+						  poi associato al protocollo in questione.
+			</p>
 			<br>
 			<p class="text-justify"><strong>Cosa significa il nome Abulafia?</strong> <br> E' il nome di un celebre uomo di cultura di alcuni secoli fa.</p>
 
