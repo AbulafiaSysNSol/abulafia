d1ecb6f434b28f471796e0ba22db739b674c577e
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-04-20 15:26:41 +0000

    indentazione pagine

--- trunk/abulafia-soloprotocollo/protocollo3-centro-include.php
+++ trunk/abulafia-soloprotocollo/protocollo3-centro-include.php
@@ -1,45 +1,62 @@
 <?php
-$annoprotocollo = $_SESSION['annoprotocollo'];
-//inizio passaggio dati da pagina inserimento
-$loginid=$_SESSION['loginid'];
-if(isset($_GET['dataoriginalegiorno'])) { $dataoriginalegiorno= $_GET['dataoriginalegiorno']; }
-if(isset($_GET['dataoriginalemese'])) { $dataoriginalemese = $_GET['dataoriginalemese']; }
-if(isset($_GET['dataoriginaleanno'])) { $dataoriginaleanno= $_GET['dataoriginaleanno']; }
-if(isset($_GET['from'])) { $from = $_GET['from']; }
-$speditaricevuta = $_POST['spedita-ricevuta'];
-$oggetto= $_POST['oggetto'];
-$data = $_POST['data'];
-$lettera_data_giorno = 0;
-$lettera_data_mese = 0;
-$lettera_data_anno = 0;
-$arraydata = explode("/", $data);
-if( isset($arraydata[0]) ) { $lettera_data_giorno = $arraydata[0]; }
-if( isset($arraydata[1]) ) { $lettera_data_mese = $arraydata[1]; }
-if( isset($arraydata[2]) ) { $lettera_data_anno = $arraydata[2]; }
-$posizione = $_POST['posizione'];
-$riferimento = $_POST['riferimento'];
-$note  = $_POST['note'];
-$urlpdf = addslashes($_GET['urlpdf']);
-$dataregistrazione = strftime("%Y-%m-%d");
-list($anno, $mese, $giorno) = explode("-", $dataregistrazione);
-$dataregistrazione2= $anno.'-'.$mese.'-'.$giorno;
-if (isset($from) && $from =='modifica') { $dataregistrazione = $dataoriginaleanno.'-'.$dataoriginalemese.'-'.$dataoriginalegiorno; }
-$idlettera=$_GET['idlettera'];
-$lettera_data = $lettera_data_anno . '-' . $lettera_data_mese . '-' . $lettera_data_giorno   ;
-$loginid = $_SESSION['loginid'];
-$auth = $_SESSION['auth'] ;
-//fine passaggio dati
+	$annoprotocollo = $_SESSION['annoprotocollo'];
+	
+	//inizio passaggio dati da pagina inserimento
+	$loginid=$_SESSION['loginid'];
+	if(isset($_GET['dataoriginalegiorno'])) { 
+		$dataoriginalegiorno= $_GET['dataoriginalegiorno']; 
+	}
+	if(isset($_GET['dataoriginalemese'])) { 
+		$dataoriginalemese = $_GET['dataoriginalemese']; 
+	}
+	if(isset($_GET['dataoriginaleanno'])) { 
+		$dataoriginaleanno= $_GET['dataoriginaleanno']; 
+	}
+	if(isset($_GET['from'])) { 
+		$from = $_GET['from']; 
+	}
+	$speditaricevuta = $_POST['spedita-ricevuta'];
+	$oggetto= $_POST['oggetto'];
+	$data = $_POST['data'];
+	$lettera_data_giorno = 0;
+	$lettera_data_mese = 0;
+	$lettera_data_anno = 0;
+	$arraydata = explode("/", $data);
+	if( isset($arraydata[0]) ) { 
+		$lettera_data_giorno = $arraydata[0]; 
+	}
+	if( isset($arraydata[1]) ) { 
+		$lettera_data_mese = $arraydata[1]; 
+	}
+	if( isset($arraydata[2]) ) { 
+		$lettera_data_anno = $arraydata[2]; 
+	}
+	$posizione = $_POST['posizione'];
+	$riferimento = $_POST['riferimento'];
+	$note  = $_POST['note'];
+	$urlpdf = addslashes($_GET['urlpdf']);
+	$dataregistrazione = strftime("%Y-%m-%d");
+	list($anno, $mese, $giorno) = explode("-", $dataregistrazione);
+	$dataregistrazione2= $anno.'-'.$mese.'-'.$giorno;
+	if (isset($from) && $from =='modifica') { 
+		$dataregistrazione = $dataoriginaleanno.'-'.$dataoriginalemese.'-'.$dataoriginalegiorno; 
+	}
+	$idlettera=$_GET['idlettera'];
+	$lettera_data = $lettera_data_anno . '-' . $lettera_data_mese . '-' . $lettera_data_giorno   ;
+	$loginid = $_SESSION['loginid'];
+	$auth = $_SESSION['auth'] ;
+	//fine passaggio dati
 
-//controllo esistenza
-$inserimento = mysql_query("UPDATE lettere$annoprotocollo set lettere$annoprotocollo.speditaricevuta ='$speditaricevuta', lettere$annoprotocollo.oggetto ='$oggetto', lettere$annoprotocollo.datalettera='$lettera_data', lettere$annoprotocollo.urlpdf='$urlpdf', lettere$annoprotocollo.posizione='$posizione', lettere$annoprotocollo.riferimento='$riferimento', lettere$annoprotocollo.note='$note', lettere$annoprotocollo.dataregistrazione='$dataregistrazione' WHERE lettere$annoprotocollo.idlettera='$idlettera'     " );
-echo  mysql_error();
-if (!$inserimento) { echo "Inserimento non riuscito" ; }
-$ultimoid = mysql_insert_id();
-$modifica =mysql_query("update joinlettereinserimento$annoprotocollo set joinlettereinserimento$annoprotocollo.idmod='$loginid', joinlettereinserimento$annoprotocollo.datamod='$dataregistrazione2' where joinlettereinserimento$annoprotocollo.idlettera='$idlettera' limit 1");
-echo  mysql_error();
-$conteggiomittenti=mysql_query("select count(*) from joinletteremittenti$annoprotocollo where idlettera='$idlettera'"); 
-$conteggiomittenti2 = mysql_fetch_row($conteggiomittenti);
-if ($conteggiomittenti2[0] < 1) { $inserimentomittenti=mysql_query("insert into joinletteremittenti$annoprotocollo values ('$idlettera',1)");}
+	//controllo esistenza
+	$inserimento = mysql_query("UPDATE lettere$annoprotocollo set lettere$annoprotocollo.speditaricevuta ='$speditaricevuta', lettere$annoprotocollo.oggetto ='$oggetto', lettere$annoprotocollo.datalettera='$lettera_data', lettere$annoprotocollo.urlpdf='$urlpdf', lettere$annoprotocollo.posizione='$posizione', lettere$annoprotocollo.riferimento='$riferimento', lettere$annoprotocollo.note='$note', lettere$annoprotocollo.dataregistrazione='$dataregistrazione' WHERE lettere$annoprotocollo.idlettera='$idlettera'     " );
+	echo  mysql_error();
+	if (!$inserimento) { echo "Inserimento non riuscito" ; }
+	$ultimoid = mysql_insert_id();
+	$modifica =mysql_query("update joinlettereinserimento$annoprotocollo set joinlettereinserimento$annoprotocollo.idmod='$loginid', joinlettereinserimento$annoprotocollo.datamod='$dataregistrazione2' where joinlettereinserimento$annoprotocollo.idlettera='$idlettera' limit 1");
+	echo  mysql_error();
+	$conteggiomittenti=mysql_query("select count(*) from joinletteremittenti$annoprotocollo where idlettera='$idlettera'"); 
+	$conteggiomittenti2 = mysql_fetch_row($conteggiomittenti);
+	if ($conteggiomittenti2[0] < 1) { $inserimentomittenti=mysql_query("insert into joinletteremittenti$annoprotocollo values ('$idlettera',1)");}
 ?>
 
 
