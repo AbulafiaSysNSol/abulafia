ce939d51b19f979ca467840dfae6bb6cad1e829f
Biagio <biagiosaitta@users.noreply.github.com>
2014-04-20 10:32:16 +0000

    aggiunto l'inserimento rapido in anagrafica da registrazione protocollo, correzione piccoli bug

--- abulafia-soloprotocollo/livesearch-cerca-mittente.php
+++ abulafia-soloprotocollo/livesearch-cerca-mittente.php
@@ -23,7 +23,7 @@
 	$my_ricerca->publricercaespolosa($q, 'cognome');
 	$where= $my_ricerca->where;
 	
-	echo '<br>' . $q . ' non presente nell\'anagrafica. <a href="#" data-toggle="modal" data-target="#myModal">Aggiungi al database</a><br>';
+	echo '<br>' . $q . ' non presente nell\'anagrafica. <a href="#" data-toggle="modal" data-target="#myModal">Inserimento Rapido</a><br>';
 	
 	$sql=mysql_query("SELECT * FROM anagrafica $where $filtro limit 5");
 	while($row = mysql_fetch_array($sql)) {
@@ -35,53 +35,6 @@
 		<br>
 		<?php
 	}
-	
-	
-	?>
-	
-	<!-- Modal -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="form" aria-labelledby="myModalLabel" aria-hidden="true">
-	  <div class="modal-dialog">
-	    <div class="modal-content">
-	    	<form method="POST" action="cazzo.php">
-	      <div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h4 class="modal-title" id="myModalLabel">Inserimento rapido in anagrafica</h4>
-	      </div>
-	      
-			<div class="modal-body">
-			
-					<div class="form-group">
-						<label>Tipologia:</label>
-						<div class="row">
-							<div class="col-xs-4">
-								<select class="form-control input-sm" name="anagraficatipologia">
-								<OPTION value="persona"> Persona Fisica</OPTION>
-								<OPTION value="carica"> Carica Elettiva o Incarico</OPTION>
-								<OPTION Value="ente"> Ente</OPTION>
-								</select>
-							</div>
-						</div>
-						<br><label>Cognome o Denominazione:</label>
-						<div class="row">
-							<div class="col-xs-8">
-								<input type="text" class="form-control input-sm" name="cognome" value="<?php echo $q; ?>">
-							</div>
-						</div>
-						<br><br>
-						<div align="right">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
-							<button type="submit" class="btn btn-primary">Salva ed associa al protocollo</button>
-						</div>
-					</div>
-				
-			</div>
-		</form>
-	    </div>
-	  </div>
-	</div>
-
-	<?php
 
 	mysql_close ($verificaconnessione);
 
