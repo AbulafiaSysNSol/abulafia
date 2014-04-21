155264f3fa0a2e36edaed4726ec5be529d07a174
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-04-21 21:04:08 +0000

    adattata la grafica della pagina modifica protocollo, corretti alcuni bug, ed inserito il livesearch anche nel modifica protocollo.

--- trunk/abulafia-soloprotocollo/dettagli-protocollo-centro-include.php
+++ trunk/abulafia-soloprotocollo/dettagli-protocollo-centro-include.php
@@ -1,78 +1,25 @@
 <?php
-$my_lettera = new Lettera(); //crea un nuovo oggetto 'lettera'
-//$_SESSION['my_lettera']= serialize($my_lettera); //serializzazione per passaggio alle variabili di sessione (se l'oggetto non viene riutilizzato è inutile serializzare?)
+	$my_lettera = new Lettera(); //crea un nuovo oggetto 'lettera'
 ?>
 
-
-	<div id="primarycontent">
-		
-			<!-- primary content start -->
-		
-			<div class="post">
-				<div class="header">
-					<h3><u>Dettagli Protocollo:</u></h3>
-				
-				</div>
-				<div class="content">
-					<p>
-
-
-<div class="content">
-<p>
-	<?php 
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title"><strong>Dettagli Protocollo: <?php echo $_GET['id']; ?></strong></h3>
+	</div>
+	
+	<div class="panel-body">
+		<?php 
 		$my_lettera -> publdisplaylettera ($_GET['id'], $_SESSION['annoricercaprotocollo']); //richiamo del metodo "mostra" dell'oggetto Lettera
-	?> 
-
-
-</p>
-
-
-</div>
-
-</p></div>
-					
-		
-		</div>
-			
-			<!-- post end -->
-
-
-<div id="primarycontent">
-		
-			<!-- secondary content start -->
-		
-			<div class="post">
-				<div class="header">
-					<h3><u>Opzioni:</u></h3>
-				
-				</div>
-				<div class="content">
-					<p>
-
-
-
-</p>
-
-
-</div>
-<div class="content">
-				<ul class="linklist">
-
-<li class="first"><a href="login0.php?corpus=modifica-protocollo&from=risultati&id=<?php echo $_GET['id'];?>"><br>Modifica questo Protocollo</a></li>
-<li><a href="login0.php?corpus=invia-newsletter&id=<?php echo $_GET['id'];?>">Invia tramite Email</a></li>					
-<li><a href="login0.php?corpus=protocollo">Nuovo inserimento PROTOCOLLO</a></li>
-</ul>
-			</div>
-<br><br>
-
-
-</p></div>
-					
-			
-<div class="footer">
-
-					
-				</div>
-		</div>
-
+		?> 
+	</div>
+  
+  	<div class="panel-heading">
+		<h3 class="panel-title"><strong>Opzioni:</strong></h3>
+	</div>
+	
+	<div class="panel-body">
+		<p><a href="login0.php?corpus=modifica-protocollo&from=risultati&id=<?php echo $_GET['id'];?>"> <span class="glyphicon glyphicon-edit"></span> Modifica questo Protocollo</a></p>
+		<p><a href="login0.php?corpus=invia-newsletter&id=<?php echo $_GET['id'];?>"> <span class="glyphicon glyphicon-envelope"></span> Invia tramite Email</a></p>					
+		<p><a href="login0.php?corpus=protocollo"> <span class="glyphicon glyphicon-plus-sign"></span> Registra nuovo Protocollo</a></p>
+	</div>
 </div>
