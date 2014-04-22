663004470b0f668b723edbd5941bd99c296f7e71
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-04-22 19:38:35 +0000

    aggiunte icone, cambiato il footer

--- trunk/abulafia-soloprotocollo/sotto-include.php
+++ trunk/abulafia-soloprotocollo/sotto-include.php
@@ -1,6 +1,6 @@
 <center><h5><small>
 	<span xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">
-	Abulafia </span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Alfio Musmarra e Biagio Saitta</span> is licensed under a:<br><br>
+	Abulafia </span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Abulafia Sys'n'Sol</span> is licensed under a:<br><br>
 	<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/it/" target="_blank">
 	<img alt="Creative Commons Attribuzione - Non commerciale - Condividi allo stesso modo 2.5 Italia License" style="border-width:0" src="http://creativecommons.org/images/public/somerights20.png" /></a>
 	</small></h5></center>
