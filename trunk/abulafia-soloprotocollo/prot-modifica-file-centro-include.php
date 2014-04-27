381591ab9b82cf5c928296927b523d3c70f0a750
alfiomusmarra <alfiomusmarra@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-04-27 21:23:04 +0000

    Adesso non viene visualizzato il protocollo incompleto, l'ultimo, che viene registrato solo per esigenze tecniche

--- trunk/abulafia-soloprotocollo/prot-modifica-file-centro-include.php
+++ trunk/abulafia-soloprotocollo/prot-modifica-file-centro-include.php
@@ -1,63 +1,66 @@
 <?php
 
-	include 'maledetti-apici-centro-include.php';
-
-	$annoprotocollo = $_SESSION['annoprotocollo'];
 	if(isset($_GET['from'])) {
 		$from = $_GET['from'];
 	}
 	else {
 		$from = '';
 	}
-	
-	if ($from != 'modifica-protocollo') {				
-		$target_path = "lettere$annoprotocollo/";
-		$idlettera=$_GET['idlettera'];
-		$target_path = $target_path . $idlettera.basename( $_FILES['uploadedfile']['name']); 
-		$urlpdf=  addslashes($idlettera.basename( $_FILES['uploadedfile']['name'])); 
-		if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-			$inserisci=mysql_query("update lettere$annoprotocollo set urlpdf='$urlpdf' where idlettera ='$idlettera'");
-			?>
-			<SCRIPT LANGUAGE="Javascript">
-			browser= navigator.appName;
-			if (browser == "Netscape")
-			window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success"; else window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success";
-			</SCRIPT>
-			<?php
-		} 
-		else {
-			?>
-			<SCRIPT LANGUAGE="Javascript">
-			browser= navigator.appName;
-			if (browser == "Netscape")
-			window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error"; else window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error";
-			</SCRIPT>
-			<?php
-		}
-	}
-	else {
-		$target_path = "lettere$annoprotocollo/";
-		$idlettera=$_GET['idlettera'];
-		$target_path = $target_path . $idlettera.basename( $_FILES['uploadedfile']['name']); 
-		$urlpdf=  addslashes($idlettera.basename( $_FILES['uploadedfile']['name'])); 
 
-		if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-			?>
+	include 'maledetti-apici-centro-include.php';
+	$my_file= new File;
+	$annoprotocollo = $_SESSION['annoprotocollo'];
+
+	
+		$target_path = "lettere$annoprotocollo/"; //setta la directory di destinazione del file da caricare
+		$idlettera=$_GET['idlettera']; //setta l'id della lettera cui attribuire gli allegati
+		$target_path = $target_path . $idlettera."/".time().".".$my_file->estensioneFile (basename( $_FILES['uploadedfile']['name'])); 
+												/*
+												aggiunge alla directory, 
+												una sotto-directory con l'id della lettera
+												e il nome del file da caricare
+												*/
+										
+		if (!is_dir("lettere$annoprotocollo/".$idlettera)) //se non esiste una directory con il l'id della lettera, la crea per ospitare gli allegati
+			{
+			mkdir("lettere$annoprotocollo/".$idlettera, 0777, true);
+			}
+			
+		if (!is_dir("lettere$annoprotocollo/"."/temp/"))//controlla se la directory TEMP esiste dentro la dir LETTERE, altrimenti la crea
+		{
+		mkdir("lettere$annoprotocollo/"."/temp/", 0777, true);
+		}
+				
+		if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {	/*
+											se lo spostamento del file va a buon fine
+											*/ 
+			$target_path2=mysql_real_escape_string($target_path);
+			echo $target_path2;
+			$inserisci=mysql_query("insert 
+						into joinlettereallegati
+						values($idlettera, $annoprotocollo, '$target_path2')
+						");			
+			?>			
 			<SCRIPT LANGUAGE="Javascript">
 			browser= navigator.appName;
 			if (browser == "Netscape")
-			window.location="login0.php?corpus=modifica-protocollo&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success"; else window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success";
+			window.location="login0.php?corpus=<?php echo $_GET['from'];?>&annoprotocollo=<?php echo $annoprotocollo;?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success"; 
+			else window.location="login0.php?corpus=<?php echo $_GET['from'];?>&annoprotocollo=<?php echo $annoprotocollo;?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=success";
 			</SCRIPT>
 			<?php
+
 		} 
-		else {
+		else {	/*
+			se lo spostamento del file NON va a buon fine
+			*/ 
+		
 			?>
 			<SCRIPT LANGUAGE="Javascript">
 			browser= navigator.appName;
 			if (browser == "Netscape")
-			window.location="login0.php?corpus=modifica-protocollo&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error"; else window.location="login0.php?corpus=protocollo2&urlpdf=<?php echo $urlpdf ; ?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error";
+			window.location="login0.php?corpus=<?php echo $_GET['from'];?>&annoprotocollo=<?php echo $annoprotocollo;?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error"; 
+			else window.location="login0.php?corpus=<?php echo $_GET['from'];?>&annoprotocollo=<?php echo $annoprotocollo;?>&idlettera=<?php echo $idlettera;?>&from=urlpdf&upfile=error";
 			</SCRIPT>
 			<?php
 		}
-	}
 ?>
