529dcb53f0d63cccee5322ed428df5009580de8a
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-05-03 14:05:24 +0000

    risolto parzialmente problema degli apici

--- trunk/abulafia-soloprotocollo/maledetti-apici-centro-include.php
+++ trunk/abulafia-soloprotocollo/maledetti-apici-centro-include.php
@@ -1,6 +1,6 @@
 <?php
 
-if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc() === 0){
+if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc() == 0){
  $_POST = array_map('addslashes', $_POST );
  $_GET = array_map('addslashes', $_GET );
  $_COOKIE = array_map('addslashes', $_COOKIE );
