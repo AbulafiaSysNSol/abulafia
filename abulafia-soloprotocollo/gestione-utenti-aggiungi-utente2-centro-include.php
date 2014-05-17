970ee33bd242ca2e916b4056277d0f7e4dff60f0
Biagio <biagiosaitta@users.noreply.github.com>
2014-05-17 15:51:27 +0000

    aggionamento grafica modifica utente

--- abulafia-soloprotocollo/gestione-utenti-aggiungi-utente2-centro-include.php
+++ abulafia-soloprotocollo/gestione-utenti-aggiungi-utente2-centro-include.php
@@ -1,36 +1,17 @@
 <?php 
-$id=$_GET['id'];
-$nome=$_GET['nome'];
-$cognome=$_GET['cognome'];
+	$id=$_GET['id'];
+	$nome=$_GET['nome'];
+	$cognome=$_GET['cognome'];
+	
+	$nomenuovoutente= strlower($nome.'.'.$cognome);
+	$passwordnuovoutente= md5($nomenuovoutente);
+	$nuovoutente=mysql_query("insert into users values('$id',0,'$nomenuovoutente', '$passwordnuovoutente')");
 ?>
-<div id="primarycontent">
-<div class="post">
-				<div class="header">
-					<h3><u>Inserisci nuovo utente:</u></h3>
-				
-				</div>
-				<div class="content">
-					<p>
 
-<?php
-$nomenuovoutente= $nome.'.'.$cognome;
-$passwordnuovoutente= md5($nomenuovoutente);
-$nuovoutente=mysql_query("insert into users values('$id',0,'$nomenuovoutente', '$passwordnuovoutente')");
-
-
-
-?>
 <SCRIPT LANGUAGE="Javascript">
-browser= navigator.appName;
-if (browser == "Netscape")
-window.location="login0.php?corpus=gestione-utenti"; else window.location="login0.php?corpus=gestione-utenti"
+	browser= navigator.appName;
+	if (browser == "Netscape")
+		window.location="login0.php?corpus=gestione-utenti"; 
+	else 
+		window.location="login0.php?corpus=gestione-utenti";
 </SCRIPT>
-
-</p></div>
-					
-
-		</div>
-			
-			<!-- post end -->
-
-</div>
