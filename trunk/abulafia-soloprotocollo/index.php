16f00f5c23a5aa5c9ea6d1cfc966658fd3f66082
bia.saitta@gmail.com <bia.saitta@gmail.com@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-06-13 18:20:49 +0000

    modifiche grafiche

--- trunk/abulafia-soloprotocollo/index.php
+++ trunk/abulafia-soloprotocollo/index.php
@@ -30,64 +30,89 @@
 <body>
 
 <div class="row">
-	<div class="col-md-6 col-md-offset-3">
-
-<div class="container-login">
-
-	<center><div class="well"><label><h1 class="paneltitle">Abulafia</h1></label><br>Gestione delle segreterie dei Volontari della CRI</div><center>
-
-<center>
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title" bgcolor="red"><b><span class="glyphicon glyphicon-log-in"></span> LOGIN</b></h3>
-  </div>
-  <div class="panel-body">
-    Per accedere al sistema, inserisci username e password:<br><br>
-     <?php
-    if($_GET['err'] == 1) {
-	?>
-	<div class="row">
-	<div class="col-xs-12">
-	<div class="alert alert-danger"> <span class="glyphicon glyphicon-remove"></span> Username o password errati</div></div></div>
-	<?php
-   }
-   ?>
- 
-    <form action="login1.php" method="post" role="form">
-  <div class="form-group">
-    <label>Username:</label>
-	<div class="row">
-	<div class="col-md-8 col-md-offset-2">
-		<input type="text" class="form-control" name="userid" placeholder="username">
+	<div class="col-md-8 col-md-offset-2">		
+		
+		<div class="container-login">
+			<center>
+				<div class="well"><label><h1 class="paneltitle">Abulafia</h1></label><br>Gestione delle segreterie dei Volontari della CRI</div>
+				<?php
+					if($_GET['err'] == 1) {
+						?>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="alert alert-danger"> <span class="glyphicon glyphicon-remove"></span> Username o password errati</div>
+							</div>
+						</div>
+						<?php
+					}
+				?>
+			</center>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title"><span class="glyphicon glyphicon-inbox"></span> <strong>Abulafia: saper conservare, saper ritrovare.</strong></h3>
+							</div>
+					
+							<div class="panel-body">
+								<div style="text-align: justify">
+									Si evolvono gli uffici, pubblici e privati, per stare al passo con i tempi e con le rinnovate esigenze di gestione amministrativa e del personale.
+									<br>Abulafia vuole essere un piccolo contributo, aperto a chiunque abbia voglia di suggerire 
+									miglioramenti, per la gestione degli affari correnti delle segreterie dei volontari della CRI.
+									<br><br>Chiunque fosse interessato al progetto, puo' contattare l'amministratore del sito all'indirizzo email <strong>informatica@cricatania.it</strong> <span class="glyphicon glyphicon-envelope"></span>
+								</div>
+								<div class="text-right">
+									<small>03 dicembre 2008</small>
+								</div>
+							</div>
+						</div>
+					</div>
+			<center>
+					<div class="col-md-6">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title"><b><span class="glyphicon glyphicon-log-in"></span> Login</b></h3>
+							</div>
+								
+							<div class="panel-body">
+								Per accedere al sistema, inserisci username e password:<br><br>
+			 
+								<form action="login1.php" method="post" role="form">
+									<div class="form-group">
+										<label>Username:</label>
+										<div class="row">
+											<div class="col-md-8 col-md-offset-2">
+												<input type="text" class="form-control" name="userid" placeholder="username">
+											</div>
+										</div>
+									</div>
+										
+									<div class="form-group">
+										<label>Password:</label>
+										<div class="row center">
+											<div class="col-md-8 col-md-offset-2">
+												<input type="password" class="form-control" name="password" placeholder="password">
+											</div>
+										</div>
+									</div>
+			  
+									<button type="submit" class="btn btn-primary"> <span class="glyphicon glyphicon-ok"></span> Accedi</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</center>
+			<hr>
+			<center>
+				<small>
+					Abulafia is licensed under a: <a href="license.txt" target="_blank">GNU GPL V.3</a><br />
+					&copy; 2008 - 2014 <strong>Abulafia Sys'n'Sol</strong>
+				</small>
+			</center>
+		</div>
 	</div>
-	</div>
-  </div>
-  <div class="form-group">
-    <label>Password:</label>
-	<div class="row center">
-	<div class="col-md-8 col-md-offset-2">
-		<input type="password" class="form-control" name="password" placeholder="password">
-	</div>
-	</div>
-  </div>
-  <button type="submit" class="btn btn-primary"> <span class="glyphicon glyphicon-ok"></span> Accedi</button>
-</form>
-
-</div>
-</div>
-</center>
-<hr>
-	<center>
-		<small>
-			Abulafia is licensed under a: <a href="license.txt" target="_blank">GNU GPL V.3</a><br />
-			&copy; 2008 - 2014 <strong>Abulafia Sys'n'Sol</strong>
-		</small>
-	</center>
-
-</div>
 </div>
 
 </body>
 </html>
-
-
