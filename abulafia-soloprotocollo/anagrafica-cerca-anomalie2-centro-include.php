7fbeabb63537bbbf446d61cc4f6dd7e28f619211
alfiomusmarra <alfiomusmarra@e011aba5-1a4f-e1b1-1f93-179f0ff5bda2>
2014-08-25 20:21:58 +0000

    Modifiche grafiche: in risultati-protocollo i file allegati vengono elencati uno per riga, in dettagli-lettera vengono elencati separati da puntoevirgola.

---
+++ abulafia-soloprotocollo/anagrafica-cerca-anomalie2-centro-include.php
@@ -0,0 +1,342 @@
+<?php
+
+	$controlloget='ok';
+	
+	//RECUPERO VARIABILE FROM DAL GET
+	
+	if(isset($_GET['filtro'])) //controlla che sia passata la variabile
+		{ 
+		$filtro= $_GET['filtro'];
+		}
+	
+	else { $controlloget='no'; }
+	
+	
+	if(isset($_GET['id1'])) //controlla che sia passata la variabile
+		{ 
+		$id1= $_GET['id1']; 
+		}
+	else { $controlloget='no'; }
+		
+	if(isset($_GET['id2'])) //controlla che sia passata la variabile
+		{ 
+		$id2= $_GET['id2'];
+	
+		}
+	else { $controlloget='no'; }
+		
+	//controlli logici
+	
+	if (($id1==$id2) or ($controlloget=='no')) //se una delle variabili manca, non prosegue
+		{
+		$_SESSION['message']= 'Errore nell\'url, ricontrollare i parametri della pagina';
+		?>
+		<script language="javascript">
+ 			<!--
+ 			document.modulo.action = 'login0.php?corpus=anagrafica-cerca-anomalie';
+ 			-->
+ 		</script>
+ 		<?php
+		exit();
+		}
+		
+
+
+	if (isset($_SESSION['my_lettera']) && ($from != 'modifica')) {
+		$my_lettera = unserialize($_SESSION['my_lettera']); //carica l'oggetto 'lettera'
+		// CONTROLLO SE E' STATO INSERITO ALMENO UN MITTENTO O UN DESTINATARIO
+		if (count($my_lettera->arraymittenti) < 1) { 
+			$_SESSION['spedita-ricevuta'] = $_POST['spedita-ricevuta'];
+			$_SESSION['oggetto'] = $_POST['oggetto'];
+			$_SESSION['data'] = $_POST['data'];
+			$_SESSION['posizione'] = $_POST['posizione'];
+			$_SESSION['riferimento'] = $_POST['riferimento'];
+			$_SESSION['pratica'] = $_POST['pratica'];
+			$_SESSION['note'] = $_POST['note'];
+			
+			//RITORNO ALLA PAGINA DI REGISTRAZIONE  O DI MODIFICA SE NON E' STATO INSERITO NEMMENO UN MITTENTO O UN DESTISTARIO
+			$_SESSION['my_lettera']=serialize ($my_lettera);
+			if($from != "modifica") {
+			?>
+		
+				<SCRIPT LANGUAGE="Javascript">
+				browser= navigator.appName;
+				if (browser == "Netscape")
+				window.location="login0.php?corpus=protocollo2&from=errore"; 
+				else window.location="login0.php?corpus=protocollo2&from=errore";
+				</SCRIPT>
+				<?php
+				exit();
+			}
+			else {
+				?>
+				<SCRIPT LANGUAGE="Javascript">
+				browser= navigator.appName;
+				if (browser == "Netscape")
+				window.location="login0.php?corpus=modifica-protocollo
+						&from=errore
+						&tabella=protocollo
+						&id=<?php echo $idlettera;?>"; 
+				else window.location="login0.php?corpus=modifica-protocollo
+							&from=errore
+							&tabella=protocollo
+							&id=<?php echo $idlettera;?>";
+				</SCRIPT>
+				<?php
+				exit();
+			}
+		}
+		// FINE CONTROLLO MITTENTI/DESTINATARI
+	}
+
+	//CREAZIONE NUOVI OGGETTI
+	$anagrafica = new Anagrafica();
+	$calendario = new Calendario();
+	
+	//LIBRERIA PER L'INVIO DI EMAIL
+	include('lib/phpmailer/PHPMailerAutoload.php');
+	$date=strftime("%d/%m/%Y");
+	$ora = date("g:i a");
+	$datamail = $date . ' alle ' . $ora;
+	
+	//VARIABILI DI SESSIONI
+	$annoprotocollo = $_SESSION['annoprotocollo'];
+	$loginid=$_SESSION['loginid'];
+	
+	//ACQUISISCO I DATI DAL FORM
+	$speditaricevuta = $_POST['spedita-ricevuta'];
+	$oggetto= $_POST['oggetto'];
+	$datalettera = $calendario->dataDB($_POST['data']);
+	$posizione = $_POST['posizione'];
+	$riferimento = $_POST['riferimento'];
+	$pratica = $_POST['pratica'];
+	$note  = $_POST['note'];
+	$dataregistrazione = strftime("%Y-%m-%d");
+	$loginid = $_SESSION['loginid'];
+	$auth = $_SESSION['auth'] ;
+	//FINE ACQUISIZIONE DATI
+
+	
+	//INSERISCO I DATI NEL DATABASE SE IL FROM E' DIVERSO DA MODIFICA
+	if($from != 'modifica') {
+	
+		//SCRIVO I DETTAGLI DELLA LETTERA NEL DB
+		$inserimento = mysql_query("insert 
+					into lettere$annoprotocollo
+					values
+					('', 
+					'$oggetto',
+					'$datalettera',
+					'$dataregistrazione',
+					'',
+					'$speditaricevuta', 
+					'$posizione', 
+					'$riferimento', 
+					'$pratica', 
+					'$note')
+					");
+		
+		$ultimoid = mysql_insert_id();
+		
+		//SCRIVO L'UTENTE CHE HA FATTO L'INSERIMENTO
+		$utentemod =mysql_query("	INSERT INTO 
+								joinlettereinserimento$annoprotocollo 
+							VALUES ( 
+								'$ultimoid',
+								'$loginid',
+								'',
+								'$dataregistrazione'
+							)
+						");
+		
+		//SCRIVO I MITTENTI/DESTINATARI NEL DB
+		foreach ($my_lettera->arraymittenti as $key => $value) { //inserisce i dati dei mittenti nel db
+			$inserimento1= mysql_query("insert
+						into joinletteremittenti$annoprotocollo
+						values
+						('$ultimoid',
+						'$key'
+						)");
+			echo  mysql_error();
+		}
+		
+		//SCRIVO GLI ALLEGATI NEL DB
+		foreach ($my_lettera->arrayallegati as $key => $value) { //inserisce i dati degli allegati nel db e provvede a spostare i file dalla dir temp
+			$inserimento2= mysql_query("insert
+						into joinlettereallegati
+						values
+						(
+						'$ultimoid',
+						'$annoprotocollo',
+						'$key')
+						");
+			echo  mysql_error();
+			if (!is_dir("lettere$annoprotocollo/".$ultimoid)) { //se non esiste una directory con il l'id della lettera, 
+									//la crea per ospitare gli allegati
+									mkdir("lettere$annoprotocollo/".$ultimoid, 0777, true);
+									}
+			rename($value, "lettere$annoprotocollo".'/'.$ultimoid.'/'.$key);
+		}
+		
+		//CREO IL QRCODE NELLA DIRECTORY LETTEREANNO/QRCODE
+		include('lib/qrcode/qrlib.php');
+		$id = $ultimoid;
+		$anno = $annoprotocollo;
+		
+		if (!is_dir('lettere'.$anno.'/qrcode/')) {
+			$creadir=mkdir('lettere'.$anno.'/qrcode/', 0777, true);
+			if (!$creadir) die ("Impossibile creare la directory: qrcode/");
+		}
+		
+		$pathqrcode = 'lettere'.$anno.'/qrcode/'.$id.$anno.'.png';
+		$param = 'Protocollo n° '.$id.' del '.$dataregistrazione;
+		$codeText = $param; 
+		$debugLog = ob_get_contents(); 
+		QRcode::png($codeText, $pathqrcode);
+		
+		//SE L'INSERIMENTO NON VA A BUON FINE SCRIVO NEL LOG L'ERRORE
+		if ( (!$inserimento || !$inserimento1) ) { 
+			echo "Inserimento non riuscito" ; 
+			$my_log -> publscrivilog( 	$_SESSION['loginname'], 
+								'TENTATA REGISTRAZIONE LETTERA '. $ultimoid, 
+								'FAILED' , 
+								'' , 
+								$_SESSION['historylog']
+							);
+		}
+		
+		//SE L'INSERIMENTO VA A BUON FINE SCRIVO NEL LOG E SE SONO ATTIVE LE NOTIFICHE MANDO EMAIL
+		else { 
+			$indirizzi = $anagrafica->getNotificationsIns();
+			if ($indirizzi) {
+				//invio notifica
+				$mail = new PHPMailer();
+				$mail->From = 'no-reply@cricatania.it';
+				$mail->FromName = 'Abulafia';
+				$mail->isHTML(true);
+				include "../mail-conf-include.php";
+				foreach ($indirizzi as $email) {
+					$mail->addAddress($email[0]);
+				}
+				$mail->Subject = 'Notifica registrazione nuova lettera in ' . $_SESSION['nomeapplicativo'];
+				$mail->Body    = 'Con la presente si notifica l\'avvenuta registrazione della lettera n. <b>' . $idlettera . 
+							'</b> avente come oggetto: <b>"'. $oggetto . '"</b>.<br>
+							Inserimento effettuato da <b>' . $_SESSION['loginname'] 
+							. '</b> il giorno ' . $datamail . '.<br><br>
+							Messaggio automatico inviato da ' . $_SESSION['nomeapplicativo'] 
+							.'.<br>Non rispondere a questa email.';
+				$esito = $mail->send();
+				//scrittura log mail
+				$my_log -> publscrivilog($_SESSION['loginname'],
+							'send notifications' , 
+							$esito ,
+							'notifica automatica - inserisci lettera', 
+							$_SESSION['maillog']);
+			}
+			//scrittura history log		
+			$my_log -> publscrivilog( $_SESSION['loginname'], 
+						'REGISTRATA LETTERA '. $ultimoid , 
+						'OK' , 
+						'' , 
+						$_SESSION['historylog']);
+		}
+	}
+	
+	if($from == 'modifica') {
+	
+		$idlettera = $_GET['idlettera'];
+		$modifica = mysql_query("	UPDATE 
+								lettere$annoprotocollo 
+							SET 
+								speditaricevuta = '$speditaricevuta', 
+								oggetto = '$oggetto',
+								datalettera = '$datalettera',
+								posizione = '$posizione',
+								riferimento = '$riferimento',
+								pratica = '$pratica',
+								note = '$note'
+							WHERE 
+								lettere$annoprotocollo.idlettera='$idlettera' 
+							LIMIT 1
+						");
+		echo mysql_error();
+		
+		$date=strftime("%Y-%m-%d");
+		//AGGIORNO L'UTENTE CHE HA FATTO LA MODIFICA
+		$utentemod =mysql_query("	UPDATE 
+								joinlettereinserimento$annoprotocollo 
+							SET 
+								joinlettereinserimento$annoprotocollo.idmod='$loginid', 
+								joinlettereinserimento$annoprotocollo.datamod='$date' 
+							WHERE 
+								joinlettereinserimento$annoprotocollo.idlettera='$idlettera' 
+							LIMIT 1
+						");
+	
+		//SE LA MODIFICA NON E' ANDATA A BUON FINE SCRIVO L'ERRORE NEL LOG
+		if (!$modifica) { 
+			echo "Modifica non riuscita" ; 
+			$my_log -> publscrivilog( $_SESSION['loginname'], 
+						'TENTATA MODIFICA LETTERA '. $idlettera , 
+						'FAILED' , 
+						'' , 
+						$_SESSION['historylog']);
+		}
+		
+		//SE LA MODIFICA E' ANDATA A BUON FINE SCRIVO NEL LOG E SE LE NOTIFICHE SONO ATTIVE MANDO L'EMAIL
+		else {
+			$indirizzi = $anagrafica->getNotificationsMod();
+			if ($indirizzi) {
+				//invio notifica
+				$mail = new PHPMailer();
+				$mail->From = 'no-reply@cricatania.it';
+				$mail->FromName = 'Abulafia';
+				$mail->isHTML(true);
+				include "../mail-conf-include.php";
+				foreach ($indirizzi as $email) {
+					$mail->addAddress($email[0]);
+				}
+				$mail->Subject = 'Notifica modifica lettera in ' . $_SESSION['nomeapplicativo'];
+				$mail->Body    = 'Con la presente si notifica la modifica della lettera n. <b>' . $idlettera . 
+							'</b> avente come oggetto: <b>"'. $oggetto . '"</b>.<br>
+							Modifica effettuata da <b>' . $_SESSION['loginname'] 
+							. '</b> il giorno ' . $datamail . '<br><br>
+							Messaggio automatico inviato da ' . $_SESSION['nomeapplicativo'] 
+							.'.<br>Non rispondere a questa email.';
+				$esito = $mail->send();
+				//scrittura log mail
+				$my_log -> publscrivilog($_SESSION['loginname'],
+							'send notifications' , 
+							$esito ,'notifica automatica - modifica lettera', 
+							$_SESSION['maillog']);
+			}
+			
+			//scrittura history log
+			$my_log -> publscrivilog( $_SESSION['loginname'], 
+						'MODIFICATA LETTERA '. 
+						$idlettera , 
+						'OK' , 
+						'' , 
+						$_SESSION['historylog']);
+			
+			$ultimoid= $idlettera;
+		}
+	}
+	
+	//RESET VARIABILI DI SESSIONE 
+	unset($_SESSION['spedita-ricevuta']);
+	unset($_SESSION['oggetto']);
+	unset($_SESSION['data']);
+	unset($_SESSION['posizione']);
+	unset($_SESSION['riferimento']);
+	unset($_SESSION['pratica']);
+	unset($_SESSION['note']);
+?>
+
+<SCRIPT LANGUAGE="Javascript">
+	browser= navigator.appName;
+	if (browser == "Netscape")
+		window.location="login0.php?corpus=protocollo4&from=<?php echo $from ?>&id=<?php echo $ultimoid ?>&anno=<?php echo $annoprotocollo ?>"; 
+	else 
+		window.location="login0.php?corpus=protocollo4&from=<?php echo $from ?>&id=<?php echo $ultimoid ?>&anno=<?php echo $annoprotocollo ?>"; 
+</SCRIPT>
