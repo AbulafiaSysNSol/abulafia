b25132942b54bb8f49b3979cfd8fd6bbbe5e1301
Biagio <biagiosaitta@users.noreply.github.com>
2014-10-17 19:07:25 +0000

    corretto bug errata visualizzazione statistiche protocollo alla cancellazione di un utente

--- abulafia-soloprotocollo/home-centro-include.php
+++ abulafia-soloprotocollo/home-centro-include.php
@@ -117,29 +117,33 @@
 						$DataSet = new pData;  
 						 
 						if ($res_lettere[0] > 0) {
-							$statsusers1=mysql_query("SELECT COUNT(joinlettereinserimento$anno.idinser) AS numerolettere, users.loginname FROM users, joinlettereinserimento$anno WHERE joinlettereinserimento$anno.idinser = users.idanagrafica AND datamod != '0000/00/00' GROUP BY users.idanagrafica ORDER BY numerolettere DESC");
+							$statsusers1=mysql_query("SELECT COUNT(joinlettereinserimento$anno.idinser) AS numerolettere, users.loginname, anagrafica.cognome, anagrafica.nome FROM anagrafica, users, joinlettereinserimento$anno WHERE users.idanagrafica = anagrafica.idanagrafica AND joinlettereinserimento$anno.idinser = users.idanagrafica AND datamod != '0000/00/00' GROUP BY users.idanagrafica ORDER BY numerolettere DESC");
+							echo mysql_error();
 							echo 'Dettaglio lettere registrate: <br>';
 							while ($statsusers2= mysql_fetch_array($statsusers1)) {
-									echo $statsusers2['numerolettere'] . ' inserite da '. $statsusers2['loginname'] . '<br>';
+									$statsusers2 = array_map('stripslashes', $statsusers2);
+									echo $statsusers2['numerolettere'] . ' inserite da '. ucwords(strtolower($statsusers2['nome'] . ' ' . $statsusers2['cognome'])) . '<br>';
 									$DataSet->AddPoint($statsusers2['numerolettere'],"Serie1");  
 									$DataSet->AddPoint($statsusers2['loginname'],"Serie2");
 							}
+
+							$DataSet->AddAllSeries();  
+							$DataSet->SetAbsciseLabelSerie("Serie2");  
+							 
+							// Initialise the graph  
+							$Test = new pChart(450,200);  
+							$Test->drawFilledRoundedRectangle(7,7,373,193,5,240,240,240);  
+							$Test->drawRoundedRectangle(5,5,375,195,5,230,230,230);  
+							  
+							// Draw the pie chart  
+							$Test->setFontProperties("lib/pchart/Fonts/tahoma.ttf",8);  
+							$Test->drawPieGraph($DataSet->GetData(),$DataSet->GetDataDescription(),150,90,110,PIE_PERCENTAGE,TRUE,50,20,5);  
+							$Test->drawPieLegend(310,15,$DataSet->GetData(),$DataSet->GetDataDescription(),250,250,250);  
+							  
+							$Test->Render("graphs/homegraph.png");  
+							
 						}
 
-						$DataSet->AddAllSeries();  
-						$DataSet->SetAbsciseLabelSerie("Serie2");  
-						 
-						// Initialise the graph  
-						$Test = new pChart(450,200);  
-						$Test->drawFilledRoundedRectangle(7,7,373,193,5,240,240,240);  
-						$Test->drawRoundedRectangle(5,5,375,195,5,230,230,230);  
-						  
-						// Draw the pie chart  
-						$Test->setFontProperties("lib/pchart/Fonts/tahoma.ttf",8);  
-						$Test->drawPieGraph($DataSet->GetData(),$DataSet->GetDataDescription(),150,90,110,PIE_PERCENTAGE,TRUE,50,20,5);  
-						$Test->drawPieLegend(310,15,$DataSet->GetData(),$DataSet->GetDataDescription(),250,250,250);  
-						  
-						$Test->Render("graphs/homegraph.png");  
 					?>
 					</p>
 	
