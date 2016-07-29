63a9fcb348bdf88712e185a55fbf410ee9263b78
biagiosaitta <biagiosaitta@hotmail.it>
2016-07-29 16:55:59 +0200

    Rimosse le icone glyphicon
    sostituite con le font awesome

---
+++ public/magazzino-documenti-centro-include.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-sm-12">
+
+		<div class="panel panel-default">
+			
+			<div class="panel-heading">
+				<h3 class="panel-title"><strong><i class="fa fa-file-text-o"></i> Documenti</strong></h3>
+			</div>
+			
+			<div class="panel-body">
+
+				
+			</div>
+		</div>
+	</div>
+	
+</div>
