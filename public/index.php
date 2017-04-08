13ff34d8a3a1e5a219902e45868fa1ae6d458cc0
biagiosaitta <biagiosaitta@hotmail.it>
2017-04-08 13:23:48 +0200

    Corretto Notice

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
     session_start();
     
-    if ($_SESSION['auth'] > 1 ) {
+    if (isset($_SESSION['auth']) && $_SESSION['auth'] > 1 ) {
         header("Location: login0.php?corpus=home");
     }
 
