943f3605a1c8b2824a1dcc48ea3827e50e87a4a6
biagiosaitta <biagiosaitta@hotmail.it>
2018-03-04 12:27:14 +0100

    Footer Change

--- public/informazioni-centro-include.php
+++ public/informazioni-centro-include.php
@@ -6,26 +6,19 @@
 		<div class="panel-body">
 			<p><strong>STAFF:</strong> 
 				<?php echo $_SESSION['nomeapplicativo'];?> e' stato inizialmente ideato da Biagio Saitta ed Alfio Musmarra
-				 ed oggi realizzato e mantenuto dagli stessi insieme a Federico D'urso e Stefano Principato.
-				 Preziose collaborazioni sono state fornite da Alfio Costanzo, Roberto Parrinello, 
-				 Laura Ferrara, Giovanni Susinni, Luigi Pistara', Laura Astolfi, Mara Basile e Marianna Schillaci.
-			</p>
-			<p><strong>LICENZA D'USO:</strong> Tutto il sistema e' stato sviluppato con software opensource quali 
-				(a solo titolo di esempio) Gedit, Gimp, Php, Mysql. A sua volta il 
-				codice di <?php echo $_SESSION['nomeapplicativo'];?> e' aperto a chiunque voglia contribuire
-				 allo sviluppo. E' anche possibile richiederne l'uso, che viene concesso gratuitamente per uso
-				  NO PROFIT a patto di citare la fonte, comunicarci eventuali migliorie apportate e renderle 
-				  gratuitamente disponibili insieme al codice originario.
+				ed oggi realizzato e mantenuto dagli stessi insieme a Federico D'urso e Stefano Principato.
+				Preziose collaborazioni sono state fornite da Alfio Costanzo, Roberto Parrinello, 
+				Laura Ferrara, Giovanni Susinni, Luigi Pistara', Laura Astolfi, Mara Basile e Marianna Schillaci.
 			</p>
 			<p><strong>GARANZIA:</strong> I creatori di <?php echo $_SESSION['nomeapplicativo'];?> 
 				sono disponibili a risolvere qualsiasi problema dovesse presentarsi con l'uso
-				 dell'applicativo. Tuttavia questo viene fornito "AS IS", senza alcuna garanzia.
-				  Chi lo installa ne accetta le condizioni di licenza e lo usa sotto la propria esclusiva responsabilita'.
+				dell'applicativo. Tuttavia questo viene fornito "AS IS", senza alcuna garanzia.
+				Chi lo installa ne accetta le condizioni di licenza e lo usa sotto la propria esclusiva responsabilita'.
 			</p>	
 			<p><strong>REQUISITI E COMPATIBILITA':</strong> <?php echo $_SESSION['nomeapplicativo'];?> e' stato pensato 
-					per poter essere 'portabile' e scalabile. Per eseguire l'applicativo e' sufficiente far
-					 girare, in locale o da remoto, un webserver in grado di interpretare il codice php. 
-					 Sono quindi supportati sistemi operativi quali Linux, Mac, FreeBSD e Windows.
+				per poter essere 'portabile' e scalabile. Per eseguire l'applicativo e' sufficiente far
+				girare, in locale o da remoto, un webserver in grado di interpretare il codice php. 
+				Sono quindi supportati sistemi operativi quali Linux, Mac, FreeBSD e Windows.
 			</p>
 			<p><strong>CONTATTI:</strong> Per scriverci potete usare l'indirizzo email <strong><?php echo $_SESSION['email'];?></strong>
 			</p>
--- public/sotto-include.php
+++ public/sotto-include.php
@@ -4,14 +4,14 @@
 		<table border="0" width="100%">
 		<tr>
 		<td align="left">
-			Abulafia <?php echo $_SESSION['version']; ?> is licensed under a: <a href="license.txt" target="_blank">GNU GPL V.3</a><br />
+			<a href="https://www.abulafiaweb.it" target="_blank">Abulafia Web <?php echo $_SESSION['version']; ?> - Smart Solutions</a> <br />
 			&copy; 2008 - 2017 <strong>Abulafia Sys'n'Sol</strong>
                     </td>
 		
 		<td align="right">
 			<a href="?corpus=home">Home Page</a> &middot;
-			<a href="?corpus=informazioni">Info</a> &middot;
-			<a href="http://wiki.abulafia.cricatania.it" target="_blank"><strong>Wiki</strong></a>
+			<a href="?corpus=informazioni">Info</a>
+			<!-- <a href="http://wiki.abulafia.cricatania.it" target="_blank"><strong>Wiki</strong></a> -->
 			<br><strong>If you want something done right, do it yourself.</strong>
 		</td>
 		</tr>
