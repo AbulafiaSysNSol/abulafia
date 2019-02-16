f19f378e9d8f13a4b43bac6e184b85ca8c153c5f
Biagio Saitta <biagiosaitta@hotmail.it>
2019-02-16 11:41:59 +0100

    Fix #61
    Fix #61

--- public/magazzino-documenti-carico-scarico-elimina-riga-documento-centro-include.php
+++ public/magazzino-documenti-carico-scarico-elimina-riga-documento-centro-include.php
@@ -3,30 +3,22 @@
 	$iddocumento = $_GET['documento'];
 	$m = new Magazzino();
 	$infodocumento = $m->getDocumentById($iddocumento);
-	$del = $m->eliminaRigaDocumento($idriga, $infodocumento[2]);
+	$del = $m->eliminaRigaDocumento($idriga, $infodocumento[2], $infodocumento[7]);
 	
 	if ($del) {
 		$my_log -> publscrivilog( $_SESSION['loginname'], 'ELIMINATA RIGA DOCUMENTO '. $iddocumento , 'OK' , $_SESSION['ip'] , $_SESSION['historylog']);
 		?>
-		<SCRIPT LANGUAGE="Javascript">
-		browser= navigator.appName;
-		if (browser == "Netscape")
+		<script language="javascript">
 			window.location="login0.php?corpus=magazzino-documenti-carico-scarico-prodotti&id=<?php echo $iddocumento; ?>&tipologia=<?php echo $infodocumento[7]; ?>"; 
-		else 
-			window.location="login0.php?corpus=magazzino-documenti-carico-scarico-prodotti&id=<?php echo $iddocumento; ?>&tipologia=<?php echo $infodocumento[7]; ?>";
-		</SCRIPT>
+		</script>
 		<?php
 	}
 	else {
 		$my_log -> publscrivilog( $_SESSION['loginname'], 'TENTATIVO DI ELIMINARE RIGA DOCUMENTO '. $iddocumento , 'FAILED' , $_SESSION['ip'] , $_SESSION['historylog']);
 		?>
-		<SCRIPT LANGUAGE="Javascript">
-		browser= navigator.appName;
-		if (browser == "Netscape")
+		<script language="javascript">
 			window.location="login0.php?corpus=magazzino-documenti-carico-scarico-prodotti&id=<?php echo $iddocumento; ?>&tipologia=<?php echo $infodocumento[7]; ?>"; 
-		else 
-			window.location="login0.php?corpus=magazzino-documenti-carico-scarico-prodotti&id=<?php echo $iddocumento; ?>&tipologia=<?php echo $infodocumento[7]; ?>";
-		</SCRIPT>
+		</script>
 		<?php
 	}	
 ?>
