aa864d06b8db550cc0cca25dc5116069fe53ccf8
Biagio Saitta <biagiosaitta@hotmail.it>
2019-03-30 20:05:05 +0100

    Fix Delete Utente

--- public/gestione-utenti-elimina-utente-centro-include.php
+++ public/gestione-utenti-elimina-utente-centro-include.php
@@ -8,18 +8,26 @@
 		$query->bindParam(':id', $id);
 		$query->execute();
 		$connessione->commit();
-		$cancellazione = true;
 	}	 
 	catch (PDOException $errorePDO) { 
 	   	echo "Errore: " . $errorePDO->getMessage();
 	   	$connessione->rollBack();
-	 	$cancellazione = false;
-	}	
-	
-	if (!$cancellazione) {
-		echo 'Impossibile compiere l\'azione richiesta';  
-		exit();
+	 	exit();
 	}
+
+	try {
+	   	$connessione->beginTransaction();
+		$query = $connessione->prepare("DELETE FROM usersettings WHERE idanagrafica = :id LIMIT 1"); 
+		$query->bindParam(':id', $id);
+		$query->execute();
+		$connessione->commit();
+	}	 
+	catch (PDOException $errorePDO) { 
+	   	echo "Errore: " . $errorePDO->getMessage();
+	   	$connessione->rollBack();
+	 	exit();
+	}	
+
 ?>
 
 <script language = "javascript">
