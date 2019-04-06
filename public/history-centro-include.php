80450bede7c3f13f41bd70ec8ad282dc58afecf3
alfio <alfiomusmarra@gmail.com>
2019-04-06 14:20:28 +0200

    [PDO] errore di sintassi e modifica logica del logo History

--- public/history-centro-include.php
+++ public/history-centro-include.php
@@ -1,15 +1,15 @@
 <div class="panel panel-default">
 	
 		<div class="panel-heading">
-		<h3 class="panel-title"><strong><span class="glyphicon glyphicon-time"></span> Log delle ultime 50 azioni</strong></h3>
+		<h3 class="panel-title"><strong><span class="glyphicon glyphicon-time"></span> Log integrale delle ultime 50 registrazioni</strong></h3>
 		</div>
 		<div class="panel-body">
 			<p>
 			<?php 
-				$my_log->publleggilog('0', '50', ' ', $_SESSION['page request']);//legge dal log
+				$my_log->publleggilog('0', '20', ' ', $_SESSION['logfile']);//legge dal log
 			?>
 			</p>
-			<br><b><a href="download.php?lud=history.log&est=log"><i class="fa fa-download"></i> Scarica il log di tutte le azioni</a></b>
+			<br><b><a href="download.php?lud=general.log&est=log"><i class="fa fa-download"></i> Scarica il log integrale di tutte le azioni</a></b>
 
 		</div>
 				
